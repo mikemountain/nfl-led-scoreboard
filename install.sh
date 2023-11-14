e6579fbe42985e573a1d07bcb4fc59cc8d80f1fd
btkircher <b.t.kircher@gmail.com>
2023-11-14 12:33:59 -0500

    Switched branches while testing

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo pip install -e python/
 cd ../../
 echo "Installing required dependencies. This may take some time (10-20 minutes-ish)..."
 git reset --hard
-git checkout master
+git checkout feat/ChangeToNBA
 git fetch origin --prune
 git pull
 sudo apt-get install libxml2-dev libxslt-dev
